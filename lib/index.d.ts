d60fa5de0a3aa72d06af4c2bed88f5b24cdb7b02
Christopher Meyer <chris@schwiiz.org>
2026-09-14 16:52:11 +0200

    tsdown

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,17 +1,20 @@
+//#region src/build-url.d.ts
 type QueryParam = boolean | string | number | null | undefined;
 type UrlOptions = {
-    queryParams?: Record<string, QueryParam | QueryParam[]>;
-    hash?: string;
-    path?: string | null;
-    returnAbsoluteUrl?: boolean;
+  queryParams?: Record<string, QueryParam | QueryParam[]>;
+  hash?: string;
+  path?: string | null;
+  returnAbsoluteUrl?: boolean;
 };
 declare const buildUrl: (inputUrl?: string | UrlOptions, options?: UrlOptions) => string;
-
+//#endregion
+//#region src/join-url-path.d.ts
 type SlashBehavior = boolean | "preserve";
 type JoinUrlPathOptions = {
-    leading?: SlashBehavior;
-    trailing?: SlashBehavior;
+  leading?: SlashBehavior;
+  trailing?: SlashBehavior;
 };
 declare const joinUrlPath: (segments: Array<string | number>, { leading, trailing }?: JoinUrlPathOptions) => string;
-
+//#endregion
 export { buildUrl, joinUrlPath };
+//# sourceMappingURL=index.d.ts.map
